3696ecaa3508834d5cb72da2238f12616c57388c
crindev001 <mktron97@gmail.com>
2024-05-14 20:23:44 -0400

    bd cambios

--- database/migrations/2024_05_14_015414_create_alumnos_table.php
+++ database/migrations/2024_05_14_015414_create_alumnos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('alumnos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('telefono');
             $table->timestamps();
         });
     }
